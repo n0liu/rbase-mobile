b7c445087fa8ae696104a2b22f02cdd22041521a
landuoduo <vv22345@163.com>
2025-12-08 22:29:47 +0800

    feat: 配置 ESLint 允许使用 any 类型

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  // 允许使用 any 类型
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
